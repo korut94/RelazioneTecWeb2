e8205ea8660123e1729dec93362e5f801cd0f449
Andrea Mantovani <andreamnt94@gmail.com>
2016-06-13 16:40:10 +0200

    Update zip script

--- script/makezip.sh
+++ script/makezip.sh
@@ -9,4 +9,5 @@ mkdir ${OUTPUT_NAME}/; \
     mkdir ${OUTPUT_NAME}/Immagini; \
     cp res/img/* ${OUTPUT_NAME}/Immagini/; \
     cp ${OUTPUT_NAME}.pdf ${OUTPUT_NAME}/Relazione.pdf; \
-    zip -r ${OUTPUT_NAME}.zip ${OUTPUT_NAME}/*
+    zip -r ${OUTPUT_NAME}.zip ${OUTPUT_NAME}/* \
+    rm -r ${OUTPUT_NAME}
